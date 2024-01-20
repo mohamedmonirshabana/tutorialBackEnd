7afc88926c8836dbf3ee47042e03791e4dd154d3
tarekmadady <t.elmadady@gmail.com>
2024-01-21 01:58:28 +0200

    PORT 8000

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.use((error, req, res, next) => {
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 //******************************* */
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App is working on ${port}`);
